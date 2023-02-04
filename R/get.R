723295b14df4060fe89a8fdc0c9d7c68ab5215a4
Daniel Scarnecchia <scarnecchia@gmail.com>
2023-02-04 18:21:19 -0500

    Updated documentation

--- R/get.R
+++ R/get.R
@@ -1,10 +1,25 @@
-#' Load data from a SAS file
+#' Load SAS data files into R
 #'
-#' @param source The source folder of the file
-#' @param file The name of the file
-#' @return A data frame read from the SAS file
+#' @description Safely load SAS7BDAT data files into R, either the entire folder
+#'   specified by `source` or a specified vector of files `file`. The loaded
+#'   data is returned as a list of [data.frames()]. This function uses
+#'   [purrr::safely()] to ensure it always succeeds. It returns a list with
+#'   components `result` and `error`. Results should be accessed with
+#'   `purrr::transpose(.x)[["error"]]`
+#'
+#' @param source A character string specifying the directory where the SAS data
+#'   files are located.
+#' @param file A character string specifying the name of the SAS data file to be
+#'   loaded. If `NULL`, all `*.sas7bdat` files in the `source` directory are
+#'   loaded.
+#' @return A list of data frames, one data frame per SAS file loaded.
+#' @importFrom purrr map safely
+#' @importFrom rlang set_names is_null
+#' @importFrom haven read_sas
+#' @importFrom fs dir_ls path
 #' @export
 #'
+
 load_data <- function(source, file=NULL) {
 
   if (rlang::is_null(file)) {
